bbb299f188b7d6201724b38ec426d1146b61b148
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-05-17 08:36:33 -0600

    Codewars

---
+++ CodeWars8kyuReturnNegative.js
@@ -0,0 +1,24 @@
+// 17 May 2023
+
+// CodeWars
+
+// JavaScript
+
+// Remove the first and last character
+
+// In this simple assignment you are given a number and have to make it negative. 
+// But maybe the number is already negative?
+
+
+function makeNegative(num) {
+  if(num > 0 ) {
+    num = num * -1
+  }
+  return num
+}
+
+
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
