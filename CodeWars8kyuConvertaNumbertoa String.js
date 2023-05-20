0684caa3b815fb8496cb83ee6e547c5347d2d11d
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-05-20 15:01:00 -0600

    Codewars

---
+++ CodeWars8kyuConvertaNumbertoa String.js
@@ -0,0 +1,22 @@
+// 20 May 2023
+
+// CodeWars
+
+// JavaScript
+
+// Summation
+
+// We need a function that can transform a number (integer) into a string.
+
+// Examples (input --> output):
+
+// 123  --> "123"
+// 999  --> "999"
+// 100 --> "-100"
+
+
+function numberToString(num) {
+  return num.toString()
+}
+
+
