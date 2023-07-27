04f26c5c2f5e9635f5d712da8c07d3456fbc488b
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-07-26 21:33:22 -0600

    BasicMathematicalOpperations

---
+++ CodeWars7kyuBasic Mathematical Operations.js
@@ -0,0 +1,41 @@
+// 26 July 2023
+
+// CodeWars
+
+// JavaScript
+
+// Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+':
+      answer = value1 + value2
+      return answer
+      break
+    case '-':
+      answer = value1 - value2
+      return answer
+      break
+    case '*':
+      answer = value1 * value2
+      return answer
+    break;
+    case '/':
+      answer = value1 / value2
+      return answer
+        break;
+    default:
+      console.log('Error in operation sign')
+  }
+}
+
+
